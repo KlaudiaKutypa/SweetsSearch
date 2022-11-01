77e0881f2c8f056b0395a280fa75bac24648406b
KlaudiaKutypa <74672013+KlaudiaKutypa@users.noreply.github.com>
2022-11-01 17:13:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// const search = document.querySelector(".search");
-// const list = document.querySelectorAll("li");
-
-// search.addEventListener("keyup", function(e){
-
-//     const input = e.target.value.toLowerCase();
-
-//     list.forEach((el) => {
-//         if(el.textContent.toLowerCase().indexOf(input) !== -1){
-//             el.style.display = "block";
-//         } else {
-//             el.style.display = "none";
-//         }
-//     })
-// } )
-
 const search = document.querySelector(".search");
 const list = document.querySelectorAll("li");
 
@@ -29,3 +13,22 @@ search.addEventListener("keyup", function(e){
         }
     })
 } )
+
+
+//SECOND OPTION
+
+// const search = document.querySelector(".search");
+// const list = document.querySelectorAll("li");
+
+// search.addEventListener("keyup", function(e){
+
+//     const input = e.target.value.toLowerCase();
+
+//     list.forEach((el) => {
+//         if(el.textContent.toLowerCase().indexOf(input) !== -1){
+//             el.style.display = "block";
+//         } else {
+//             el.style.display = "none";
+//         }
+//     })
+// } )
